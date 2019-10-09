bc7c0a55c05c3d1c2d9cf6ece16eeb2f19ac3aca
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-09 19:05:55 -0400

    progress

--- config/placeholderapi/javascript/combatxpcur.js
+++ config/placeholderapi/javascript/combatxpcur.js
@@ -6,7 +6,9 @@ var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
   var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
   var classData = character.getClasses().get(className); //gets class data from character
   if(classData !== null) { //checks if classData returns null
-    var out = classData.getClassDefinition().getLevelProgression().getLevelMargins();
+    var currentexp = classData.getExperiencesFromLevel();
+
+	var out = Math.round(currentexp);
 
 	return out;
   }
