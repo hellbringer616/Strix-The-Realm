f9939065b5021ca61ef147235baddd65c18cbc1b
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-09 19:17:01 -0400

    Maybe?

---
+++ config/placeholderapi/javascript/metallurgyxpcur.js
@@ -0,0 +1,16 @@
+//loads NTRPG Plugin
+var main = function() {
+var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
+  var className = "Metallurgy" //must be a string
+  var uid = player.getUniqueId(); //Get Player UUID
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
+  var classData = character.getClasses().get(className); //gets class data from character
+  if(classData !== null) { //checks if classData returns null
+    var currentexp = classData.getExperiencesFromLevel();
+
+	var out = Math.round(currentexp);
+
+	return out;
+  }
+}
+main();
