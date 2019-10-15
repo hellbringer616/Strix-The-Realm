2e885d1201efff76c1ce42a7ae0863f9a181860d
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-15 12:44:01 -0400

    Update ItemizerPoolsDrop.js
    This is a variable so it SHOULD just work

--- CustomNPCs Scripts/ItemizerPoolsDrop.js
+++ CustomNPCs Scripts/ItemizerPoolsDrop.js
@@ -8,5 +8,5 @@ function dies(event) {
   //Fetches from the Itemizer pools.conf file using the numarical string ID and sets it as the variable DroppedItem
   var DroppedItem = event.player.getWorld().createItem(ItemService.fetch(1),0,1);
   //Sets the dropped item in slot 8 of CustomNPC to drop the itemizer item 100% of the time.
-  event.npc.setDropedItem(8, DroppedItem(), 100)
+  event.npc.setDropedItem(8, DroppedItem, 100)
 }
