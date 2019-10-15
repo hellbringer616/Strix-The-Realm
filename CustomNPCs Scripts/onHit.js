dda9c42de5a68aadcc0b911d1946470882a4aa10
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-15 12:43:31 -0400

    Delete onHit.js
    Depricated

--- CustomNPCs Scripts/onHit.js
+++
@@ -1,7 +0,0 @@
-function damaged(event) {
-  var attackerName = event.source.getName();
-  var arrayHasStruck = getTempData(array);
-  if(!arrayHasStruck.includes(attackerName())){
-    arrayHasStruck.push (attackerName());
-  }
-}
