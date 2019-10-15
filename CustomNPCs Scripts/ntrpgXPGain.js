09869ea3384c4cdba2dbcff3002735cbbebdfb0c
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-14 22:21:06 -0400

    XP gain
    Set up a wonderful XP script thanks to Ghughes!

--- CustomNPCs Scripts/ntrpgXPGain.js
+++ CustomNPCs Scripts/ntrpgXPGain.js
@@ -1,10 +1,29 @@
+function init(event) {
+   var playerNamesArray = [];
+   event.npc.tempdata.put("playerNamesArray",playerNamesArray); //opens a blank array and sets it to string; needed to retrieve global data
+}
+
+function damaged(event) {
+    var playerArray = event.npc.tempdata.get("playerNamesArray"); //retrieves global data from string tag
+    var playerName = event.source.getName();
+    var index = playerArray.indexOf(playerName); //creates index of playernames
+   if(index == -1){
+      playerArray.push(playerName);
+   }
+}
+
 //On NPC death
 function died(event) {
     //set variables to get the Player Name that last damaged the NPC and the NPC's Maximum Health
+
+    var playerArray = event.npc.tempdata.get("playerNamesArray");
     var attackerName = event.source.getName();
     var maxHealth = event.npc.getMaxHealth();
     //if the death event source and it's type are PLAYER run the command, else null
     if(event.source && event.source.getType() == EntityType_PLAYER){
-        event.npc.executeCommand("nadmin exp add " + attackerName + " " + maxHealth + " PVE Combat");
+        for(var i = 0; i < playerArray.length; i++) {
+        var xp = maxHealth / 2 / playerArray.length;
+        event.npc.executeCommand("nadmin exp add " +  playerArray[i] + " " + xp + " PVE Combat");
+     }
   }
 }
