d0c417ad45ea314421c27a812c9ce2c23f448d79
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-19 13:11:16 -0400

    character menu changes
    Added Thieving skill, removed seperate buttons to acess the Crafting menus and made it a right click option for Armorer and Weaponsmithing

---
+++ config/placeholderapi/javascript/thievingxpnext.js
@@ -0,0 +1,17 @@
+//loads NTRPG Plugin
+var main = function() {
+var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
+  var className = "Thieving" //must be a string
+  var uid = player.getUniqueId(); //Get Player UUID
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
+  var classData = character.getClasses().get(className); //gets class data from character
+  if(classData !== null) { //checks if classData returns null
+    var levels = classData.getClassDefinition().getLevelProgression().getLevelMargins();
+    var expnextlevel = levels[classData.getLevel()]
+
+	var out = Math.round(expnextlevel);
+
+	return out;
+  }
+}
+main();
