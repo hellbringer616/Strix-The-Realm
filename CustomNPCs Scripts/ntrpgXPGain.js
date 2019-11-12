ec6951a29c342ea92f4a51888382c11460a2ba09
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-11 19:41:56 -0500

    Update ntrpgXPGain.js
    Refactored to remove an unneeded check and add a check to verify the the damage source is a player when adding to the array.

--- CustomNPCs Scripts/ntrpgXPGain.js
+++ CustomNPCs Scripts/ntrpgXPGain.js
@@ -4,11 +4,13 @@ function init(event) {
 }
 
 function damaged(event) {
-    var playerArray = event.npc.tempdata.get("playerNamesArray"); //retrieves global data from string tag
-    var playerName = event.source.getName();
-    var index = playerArray.indexOf(playerName); //creates index of playernames
-   if(index == -1){
-      playerArray.push(playerName);
+  if(event.source && event.source.getType() == EntityType_PLAYER) {
+      var playerArray = event.npc.tempdata.get("playerNamesArray"); //retrieves global data from string tag
+      var playerName = event.source.getName();
+      var index = playerArray.indexOf(playerName); //creates index of playernames
+     if(index == -1){
+        playerArray.push(playerName);
+     }
    }
 }
 
@@ -17,13 +19,10 @@ function died(event) {
     //set variables to get the Player Name that last damaged the NPC and the NPC's Maximum Health
 
     var playerArray = event.npc.tempdata.get("playerNamesArray");
-    var attackerName = event.source.getName();
     var maxHealth = event.npc.getMaxHealth();
     //if the death event source and it's type are PLAYER run the command, else null
-    if(event.source && event.source.getType() == EntityType_PLAYER){
         for(var i = 0; i < playerArray.length; i++) {
         var xp = maxHealth / 2 / playerArray.length;
         event.npc.executeCommand("nadmin exp add " +  playerArray[i] + " " + xp + " PVE Combat");
-     }
   }
 }
