a88b2b6264ae620b0112ae556889614641c595ef
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-12 21:09:32 -0500

    Update thieving_config.js
    added comments and fixed it

--- CustomNPCs Scripts/Copy-Paste Scripts/thieving_config.js
+++ CustomNPCs Scripts/Copy-Paste Scripts/thieving_config.js
@@ -3,7 +3,7 @@ function init(event){
         var requiredLevel = 1 //Must be an integer
         var xP = 8 //Must be an integer
         var response = "Beg your Pardon?!"//Must be a string
-        var loot = "retrieve 9999 3" //Must be a string: Use Itemizer function (retrieve for single item, fetch for item pool) itemID from Itemizer, and quantity
+        var loot = "retrieve 9999 3 " //Must be a string: Use Itemizer function (retrieve for single item, fetch for item pool) itemID from Itemizer, and quantity **MUST HAVE A LEADING SPACE**
         /////CONFIG END/////
     event.npc.tempdata.put("requiredLevel",requiredLevel);
     event.npc.tempdata.put("xP",xP);
