70564ac0ba3bfcdfe37be1a1df5179bf39b88031
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-12 20:59:10 -0500

    thieving update
    Generalized the Thieving skill code.
    Requires a Copy/Paste script currently; however this can be changed to be whatever NPC set and loaded as a script

--- CustomNPCs Scripts/thieving.js
+++ CustomNPCs Scripts/thieving.js
@@ -2,11 +2,6 @@
 var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
 //On interact with NPC
 function interact(event) {
-  ///// Config Start
-  var requiredLevel = 1 //level required to steal
-  var xP = 8 //XP per successful steal
-  var response = "Beg your Pardon?!"
-  ///// Config End
   var sneak = event.player.isSneaking(); //is the player sneaking
   var className = "Thieving" //must be a string
   var uid = event.player.getUUID(); //Get Player UUID string
@@ -15,16 +10,20 @@ function interact(event) {
   var classData = character.getClasses().get(className) //gets class data from character
 
   if (classData !== null) { //checks if classData returns null
+    var requiredLevel = event.npc.tempdata.get("requiredLevel") //level required to steal from event.npc.tempdata.put()
     var classLevel = classData.getLevel(); //returns Class Level from Class Data
     var playerName = event.player.getName(); //gets the interacting players name
     if(classLevel >= requiredLevel){
       if(sneak && event.player.getPotionEffect(2) != 6){ //checks if the player is sneaking and if the player is not inflected with slowness strength 6 and is atleast the required level to steal from the NPC
+        var xP = event.npc.tempdata.get("xP") //XP per successful steal from event.npc.tempdata.put()
         var rng = Math.random(); //generates a random number between 0 and 1
         var roll = ((5/833)*((classLevel/requiredLevel)+(classLevel-requiredLevel)))+(17/49)+rng
         if(roll >= 1){
-          event.npc.executeCommand("retrieve 9999 3 " + playerName + ""); //retrieve itemizer item of quantity and give to player
+          var loot = event.npc.tempdata.get("loot");
+          event.npc.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
           event.npc.executeCommand("nadmin exp add " + playerName + " " + xP + " " + className + " " + className); //gives the player theiving XP
         }else{
+          var response = event.npc.tempdata.get("response") //gets response from event.npc.tempdata.put()
           event.npc.say(response);
           event.player.damage(2);
           event.player.addPotionEffect(2, 3, 6, true); //adds slowness of 3 seconds of level 6 (immobile) to player
