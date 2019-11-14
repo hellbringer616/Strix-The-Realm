1c2cb31d59528c03dffb66cb929e0518bb855cbc
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-14 00:20:28 -0500

    Update ore.js
    fixes galore

--- CustomNPCs Scripts/ore.js
+++ CustomNPCs Scripts/ore.js
@@ -1,3 +1,4 @@
+var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
 function init(event){
   //set block model to stone
      event.block.setModel("minecraft:stone")
@@ -8,11 +9,16 @@ function init(event){
 }
 //When block is left clicked
 function clicked(event){
+  var className = "Metallurgy" //must be a string
+  var uid = event.player.getUUID(); //Get Player UUID string
+  var JavaUUID = Java.type('java.util.UUID'); //Magic?
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)) //load NT-RPG Character service and get UUID than convert to Java UUID
+  var classData = character.getClasses().get(className) //gets class data from character
   var heldItem = event.player.mainhandItem.displayName; //return the display name of a held item
   var isBronzePick = heldItem.indexOf("Bronze Pick") != -1; //checks if held item has the display name of "bronze Pick"
   var isIronPick = heldItem.indexOf("Iron Pick") != -1; //checks if held item has the display name of "Iron Pick"
   var health = event.block.tempdata.get("health");
-  var classData = character.getClasses().get("Metallurgy") //gets class data from NT-RPG character
+  var classData = character.getClasses().get(className) //gets class data from NT-RPG character
 
   if(classData !== null) { //checks if classData returns null
     var classLevel = classData.getLevel()
@@ -37,7 +43,7 @@ function clicked(event){
         }
           if (isSteelPick){
             var damage = (classlevel/10)*1.15
-            var health = health damage
+            var health = health - damage
           event.block.tempdata.put("health",health)
         }
           if (isMithrilPick){
