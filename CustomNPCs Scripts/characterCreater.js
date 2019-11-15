17be0cd5933ad5e2715a6eb057957acd5089bf67
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-15 13:08:31 -0500

    Update characterCreater.js
    fixed maybe?

--- CustomNPCs Scripts/characterCreater.js
+++ CustomNPCs Scripts/characterCreater.js
@@ -1,25 +1,34 @@
 //this is a player script and will not function if run anywhere other than the player
-function init(event){
-  var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
-  var uid = event.player.getUUID(); //Get Player UUID string
-  var JavaUUID = Java.type('java.util.UUID'); //Magic?
-  var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
-  var playerName = event.player.getName();
+var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
+var JavaUUID = Java.type('java.util.UUID'); //Magic?
 
-function joinClasses(join){
+
+function interact(event){
+  var uid = event.player.getUUID(); //Get Player UUID string
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
+  event.player.tempdata.put("character",character);
+  var playerName = event.player.getName();
+  event.player.tempdata.put("playerName",playerName);
+
+
+    if(character.getName() != playerName){
+        event.API.executeCommand(event.player.world, "nadmin invoke "+playerName+" char create "+playerName+"");
+    }
+    if(character.getClasses() == "{}"){
+        event.API.executeCommand(event.player.world, "nadmin invoke "+playerName+" char switch "+playerName+"");
+        event.player.timers.start(1, 40, false);
+        }
+    }
+
+function timer(event){
+  var character = event.player.tempdata.get("character");
+  var playerName = event.player.tempdata.get("playerName");
   var classesArray =["Alethi","Armorer","Combat","Metallurgy","Thieving","Weaponsmith"]
+
     for(var i = 0; i < classesArray.length; i++){
         if(character.getClasses().get(classesArray[i]) == null){
         event.API.executeCommand(event.player.world, "nadmin invoke "+playerName+" char choose class "+classesArray[i]+"");
       }
     }
-  }
-
-    if(character.getName() != playerName){
-        event.API.executeCommand(event.player.world, "nadmin invoke "+playerName+" char create "+playerName+"");
-        event.API.executeCommand(event.player.world, "nadmin invoke "+playerName+" char switch "+playerName+"");
-    }else{
-      joinClasses();
-      event.player.message("&aCharacter setup correctly!");
-    }
+    event.player.timers.stop(1);
 }
