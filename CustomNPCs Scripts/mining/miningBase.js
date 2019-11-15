b5bd6d8ff0d4580de893dd0be6dc44a3343e2111
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-14 22:44:44 -0500

    Update miningBase.js
    My first function! 8)

--- CustomNPCs Scripts/mining/miningBase.js
+++ CustomNPCs Scripts/mining/miningBase.js
@@ -3,7 +3,8 @@ var JavaUUID = Java.type('java.util.UUID'); //Magic?
 //When block is left clicked
 function clicked(event){
   var uid = event.player.getUUID(); //Get Player UUID string
-  //**// NT-RPG stuff
+
+  //** NT-RPG stuff
   var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
   var className = "Metallurgy"; //must be a string
   var classData = character.getClasses().get(className) //gets class data from character
@@ -11,18 +12,28 @@ function clicked(event){
   var xP = event.block.tempdata.get("xP");
   //**//
 
-  //***// load config from initialize tempdata
+  //*** load config from initialize tempdata
   var model = event.block.tempdata.get("model");
   var texture = event.block.tempdata.get("texture");
   var durability = event.block.tempdata.get("durability");
   var loot = event.block.tempdata.get("loot");
   //***//
+
   var heldItem = event.player.mainhandItem.displayName; //return the display name of a held item
   var isBronzePick = heldItem.indexOf("Bronze Pick") != -1; //checks if held item has the display name of "bronze Pick"
   var isIronPick = heldItem.indexOf("Iron Pick") != -1; //checks if held item has the display name of "Iron Pick"
   var isSteelPick = heldItem.indexOf("Steel Pick") != -1; //checks if held item has the display name of "Steel Pick"
   var isMithrilPick = heldItem.indexOf("Mithril Pick") != -1; //checks if held item has the display name of "Mithril Pick"
   var playerName = event.player.getName();
+
+  ///*** function that runs once a block is "mined"
+  function isMined(mined){
+    event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
+    event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy") //execute command to add experience to a class
+    event.block.setModel("minecraft:stone"); //set block model to regular stone. NOTE: Does not work with Optifine.
+    event.block.timers.start(1, 2400, false); //sets a timer that executes a function after 2 minutes
+  }
+  ///***///
 
   if(classData !== null) { //checks if classData returns null
     var classLevel = classData.getLevel(); //gets level from defined Character Class
@@ -39,10 +50,7 @@ function clicked(event){
             var durability = durability - damage;
             event.block.tempdata.put("durability",durability);
             if(durability < 1){
-              event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
-              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
-              event.block.setModel("minecraft:stone");
-              event.block.timers.start(1, 2400, false);
+              isMined();
             }
         }
           if (isIronPick){
@@ -50,10 +58,7 @@ function clicked(event){
             var durability = durability - damage;
             event.block.tempdata.put("durability",durability);
             if(durability < 1){
-              event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
-              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
-              event.block.setModel("minecraft:stone");
-              event.block.timers.start(1, 2400, false);
+              isMined();
             }
         }
           if (isSteelPick){
@@ -61,10 +66,7 @@ function clicked(event){
             var durability = durability - damage;
             event.block.tempdata.put("durability",durability);
             if(durability < 1){
-              event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
-              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
-              event.block.setModel("minecraft:stone");
-              event.block.timers.start(1, 2400, false);
+              isMined();
             }
         }
           if (isMithrilPick){
@@ -72,16 +74,13 @@ function clicked(event){
             var durability = durability - damage;
             event.block.tempdata.put("durability",durability);
             if(durability < 1){
-              event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
-              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
-              event.block.setModel("minecraft:stone");
-              event.block.timers.start(1, 2400, false);
+              isMined();
             }
         }
-            //**
+            //**//
         }else{
         event.player.message("&eYou need a Pickaxe to mine.");
-        //*
+        //*//
         }
       }
   }else{
