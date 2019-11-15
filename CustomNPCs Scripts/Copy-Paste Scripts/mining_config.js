a4dd07c78dd7aedf3f879c268e615a812b0224a0
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-14 19:25:49 -0500

    Minning
    My goodness my script is ugly...

---
+++ CustomNPCs Scripts/Copy-Paste Scripts/mining_config.js
@@ -0,0 +1,16 @@
+function init(event){
+    var model = "minecraft:stone" //must be a string; is the block ID of the model
+    var texture = 3 //Optional; must be an integer; interger is equal to the block varient.
+    var durability = 20;
+    var loot = "retrieve 1 1 "; //Must be a string: Use Itemizer function (retrieve for single item, fetch for item pool) itemID from Itemizer, and quantity **MUST HAVE A LEADING SPACE**
+    var requiredLevel = 1; //Must be an integer
+    var xP = 7.5 //must be an integer
+        event.block.setModel(model); //sets the model of the block
+        event.block.model.setItemDamage(texture); //sets the texture of the block
+        event.block.tempdata.put("model",model);
+        event.block.tempdata.put("texture",texture);
+        event.block.tempdata.put("durability",durability);
+        event.block.tempdata.put("loot",loot);
+        event.block.tempdata.put("requiredLevel",requiredLevel);
+        event.block.tempdata.put("xP",xP);
+}
--- CustomNPCs Scripts/miningOre.js
+++ CustomNPCs Scripts/miningOre.js
@@ -1,13 +1,5 @@
 var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin"); //load NT-RPG API
 var JavaUUID = Java.type('java.util.UUID'); //Magic?
-function init(event){
-  //set block model to stone
-     event.block.setModel("minecraft:stone");
-  //set texture to 1 (granite)
-     event.block.model.setItemDamage(1);
-     var health = 20;
-     event.block.tempdata.put("health",health);
-}
 //When block is left clicked
 function clicked(event){
   var uid = event.player.getUUID(); //Get Player UUID string
@@ -15,63 +7,68 @@ function clicked(event){
   var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
   var className = "Metallurgy"; //must be a string
   var classData = character.getClasses().get(className) //gets class data from character
-  var requiredLevel = 1 //must be an interger
+  var requiredLevel = event.block.tempdata.get("requiredLevel");
+  var xP = event.block.tempdata.get("xP");
   //**//
   var heldItem = event.player.mainhandItem.displayName; //return the display name of a held item
   var isBronzePick = heldItem.indexOf("Bronze Pick") != -1; //checks if held item has the display name of "bronze Pick"
   var isIronPick = heldItem.indexOf("Iron Pick") != -1; //checks if held item has the display name of "Iron Pick"
   var isSteelPick = heldItem.indexOf("Steel Pick") != -1; //checks if held item has the display name of "Steel Pick"
   var isMithrilPick = heldItem.indexOf("Mithril Pick") != -1; //checks if held item has the display name of "Mithril Pick"
-  var health = event.block.tempdata.get("health");
-  var loot = "retrieve 1 1 "; //Must be a string: Use Itemizer function (retrieve for single item, fetch for item pool) itemID from Itemizer, and quantity **MUST HAVE A LEADING SPACE**
+  var durability = event.block.tempdata.get("durability");
+  var loot = event.block.tempdata.get("loot");
   var playerName = event.player.getName();
 
   if(classData !== null) { //checks if classData returns null
     var classLevel = classData.getLevel(); //gets level from defined Character Class
 
-      //if health is less than 1 change model to stone and starts a timer
-      if(health < 1){
-          event.player.message("&eYou cannot mint this right now."); //does not work
+      //if durability is less than 1 change model to stone and starts a timer
+      if(durability < 1){
+          event.player.message("&eYou cannot mine this right now."); //does not work
         }else{
           //* checks to see if the item is a pickaxe; otherwise tells the player they cannot mine
         if (heldItem.indexOf("Pick") && classLevel >= requiredLevel){
             //** Checks which pickaxe the player is holding and adjusts damage accordingly based on class level and pickaxe type.
           if (isBronzePick){
             var damage = 1+(classLevel/10)*1;
-            var health = health - damage;
-            event.block.tempdata.put("health",health);
-            if(health < 1){
+            var durability = durability - damage;
+            event.block.tempdata.put("durability",durability);
+            if(durability < 1){
               event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
+              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
               event.block.setModel("minecraft:stone");
               event.block.timers.start(1, 2400, false);
             }
         }
           if (isIronPick){
             var damage = 1+(classLevel/10)*1.1;
-            var health = health - damage;
-            event.block.tempdata.put("health",health);
-            if(health < 1){
+            var durability = durability - damage;
+            event.block.tempdata.put("durability",durability);
+            if(durability < 1){
               event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
+              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
               event.block.setModel("minecraft:stone");
               event.block.timers.start(1, 2400, false);
             }
         }
           if (isSteelPick){
             var damage = 1+(classLevel/10)*1.15;
-            var health = health - damage;
-            event.block.tempdata.put("health",health);
-            if(health < 1){
+            var durability = durability - damage;
+            event.block.tempdata.put("durability",durability);
+            if(durability < 1){
               event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
+              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
               event.block.setModel("minecraft:stone");
               event.block.timers.start(1, 2400, false);
             }
         }
           if (isMithrilPick){
             var damage = 1+(classLevel/10)*1.2;
-            var health = health - damage;
-            event.block.tempdata.put("health",health);
-            if(health < 1){
+            var durability = durability - damage;
+            event.block.tempdata.put("durability",durability);
+            if(durability < 1){
               event.block.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
+              event.block.executeCommand("nadmin exp add " + playerName + " " + xP + " " + " metallurgy metallurgy")
               event.block.setModel("minecraft:stone");
               event.block.timers.start(1, 2400, false);
             }
@@ -82,12 +79,14 @@ function clicked(event){
         //*
         }
       }
+  }else{
+    event.player.message("&cYOU ARE NOT IN A CLASS")
   }
 }
-//after the timer is triggered; sets the block texture to Granite and sets health back to 20
+//after the timer is triggered; sets the block texture to Granite and sets durability back to 20
 function timer(event){
      event.block.setModel("minecraft:stone");
      event.block.model.setItemDamage(1);
-   var health = 20;
-   event.block.tempdata.put("health",health);
+   var durability = 20;
+   event.block.tempdata.put("durability",durability);
 }
