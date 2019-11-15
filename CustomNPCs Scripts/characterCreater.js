4b8b29922765e5a7e167ea53d9f0f6730d385670
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-15 00:59:48 -0500

    Create characterCreater.js
    script to make an NPC force player to join classes.

---
+++ CustomNPCs Scripts/characterCreater.js
@@ -0,0 +1,24 @@
+function interact(event){
+  var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
+  var uid = event.player.getUUID(); //Get Player UUID string
+  var JavaUUID = Java.type('java.util.UUID'); //Magic?
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
+  var playerName = event.player.getName();
+
+function joinClasses(join){
+  var classesArray =["Alethi","Armorer","Combat","Metallurgy","Thieving","Weaponsmith"]
+    for(var i = 0; i < classesArray.length; i++){
+        if(character.getClasses().get(classesArray[i]) == null){
+        event.npc.executeCommand("nadmin invoke "+playerName+" char choose class "+classesArray[i]+"");
+      }
+    }
+}
+
+    if(character.getName() != playerName){
+        event.npc.executeCommand("nadmin invoke "+playerName+" char create "+playerName+"");
+        event.npc.executeCommand("nadmin invoke "+playerName+" char switch "+playerName+"");
+        delay(2, joinClasses());
+    }else{
+      joinClasses();
+    }
+}
