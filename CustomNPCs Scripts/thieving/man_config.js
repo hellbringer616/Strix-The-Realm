413517193545399a8f990e1b5bebbfc68ce164ba
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-17 15:08:47 -0500

    thieving scripts
    Upated thieving scripts to use Total Economy in an attempt to move away from item based (visual) economy

--- CustomNPCs Scripts/thieving/man_config.js
+++ CustomNPCs Scripts/thieving/man_config.js
@@ -3,10 +3,12 @@ function init(event){
         var requiredLevel = 1 //Must be an integer
         var xP = 8 //Must be an integer
         var caughtResponse = "Beg your Pardon?!"//Must be a string
-        var loot = "retrieve 9999 3 " //Must be a string: Use Itemizer function (retrieve for single item, fetch for item pool) itemID from Itemizer, and quantity **MUST HAVE A LEADING SPACE**
+        var econLoot = 3 //amount of coins you wish to give the player after a successful steal
+        //var loot = "retrieve 9999 3 " //Must be a string: Use Itemizer function (retrieve for single item, fetch for item pool) itemID from Itemizer, and quantity **MUST HAVE A LEADING SPACE**
         /////CONFIG END/////
     event.npc.tempdata.put("requiredLevel",requiredLevel);
     event.npc.tempdata.put("xP",xP);
     event.npc.tempdata.put("caughtResponse",caughtResponse);
-    event.npc.tempdata.put("loot",loot);
+    event.npc.tempdata.put("econLoot","econLoot")
+    //event.npc.tempdata.put("loot",loot);
 }
--- CustomNPCs Scripts/thieving/thievingBase.js
+++ CustomNPCs Scripts/thieving/thievingBase.js
@@ -20,9 +20,12 @@ function interact(event) {
         var rng = Math.random(); //generates a random number between 0 and 1
         var roll = ((5/833)*((classLevel/requiredLevel)+(classLevel-requiredLevel)))+(17/49)+rng
         if(roll >= 1){
-          var loot = event.npc.tempdata.get("loot");
-          event.npc.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
+          var econLoot = event.npc.tempdata.get("econLoot") //gets econLoot from event.npc.tempdata.put()
+          event.npc.executeCommand("adminpay " + playerName = " " econLoot);
+          //var loot = event.npc.tempdata.get("loot");
+          //event.npc.executeCommand(loot + playerName); //retrieve itemizer item of quantity and give to player
           event.npc.executeCommand("nadmin exp add " + playerName + " " + xP + " " + className + " " + className); //gives the player theiving XP
+          event.player.message("&eYou have stolen " + "&c" + econLoot + "&ecoins.");
         }else{
           var caughtResponse = event.npc.tempdata.get("caughtResponse") //gets caughtResponse from event.npc.tempdata.put()
           event.npc.say(caughtResponse);
@@ -30,10 +33,10 @@ function interact(event) {
           event.player.addPotionEffect(2, 3, 6, true); //adds slowness of 3 seconds of level 6 (immobile) to player
         }
       }else{
-        event.npc.say("Hello Adventurer");
+        event.player.message("&eYou are stunned.");
       }
     }else{
-      event.player.message(" &eYou must be at least level " + "&4" + requiredLevel + " &eto steal");
+      event.player.message("&eYou must be at least level " + "&4" + requiredLevel + " &eto steal");
     }
   }
 }
