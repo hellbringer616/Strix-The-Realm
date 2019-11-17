3905cf37af37519b5e7ba37b6f43e6710759a63c
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-16 19:07:28 -0500

    thieving update
    updating thieving scripts to match the folder structure of mining

---
+++ CustomNPCs Scripts/thieving/guard_config.js
@@ -0,0 +1,12 @@
+function init(event){
+        /////CONFIG START/////
+        var requiredLevel = 20 //Must be an integer
+        var xP = 24.5 //Must be an integer
+        var response = "Beg your Pardon?!"//Must be a string
+        var loot = "retrieve 9999 20 " //Must be a string: Use Itemizer function (retrieve for single item, fetch for item pool) itemID from Itemizer, and quantity **MUST HAVE A LEADING SPACE**
+        /////CONFIG END/////
+    event.npc.tempdata.put("requiredLevel",requiredLevel);
+    event.npc.tempdata.put("xP",xP);
+    event.npc.tempdata.put("response",response);
+    event.npc.tempdata.put("loot",loot);
+}
