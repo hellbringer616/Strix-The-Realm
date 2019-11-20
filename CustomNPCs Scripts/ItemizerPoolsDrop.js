340445ad0cf4773c921cd736ff28204b366c0f5a
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-20 10:19:50 -0500

    Update ItemizerPoolsDrop.js
    magic itemizer drop tables?

--- CustomNPCs Scripts/ItemizerPoolsDrop.js
+++ CustomNPCs Scripts/ItemizerPoolsDrop.js
@@ -1,13 +1,9 @@
-//script that runs on death and retrieve and item from itemizer itempool
-//**NOTE** syntax for this will likely be all wrong; I need to keep better notes...
-
 //Loads a seperate API, in this case Itemizers ItemService
 var itemService = Java.type("com.onaple.itemizer.utils.ItemService");
-var spongeItemStack = Jave.type("org.spongepowered.api.item.inventory.ItemStack");
 //Anything within the brackets runs on the death of an NPC
 function dies(event) {
-  //Fetches from the Itemizer pools.conf file using the numarical string ID and creates an IitemStack with size of 1 and sets it as the variable DroppedItem
-  var dropedItems = event.player.getWorld().createItem(spongeItemStack.itemService.fetch(1),0,1);
-  //Sets the dropped item in slot 8 of CustomNPC to drop the itemizer item 100% of the time.
-  event.npc.setDropedItem(8, dropedItems, 100)
+//fetch the pool at each death
+var item = itemService.fetch(1)
+  // Instanciate the item at the location where the NPC dies
+itemService.instanciate(item,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ());
 }
