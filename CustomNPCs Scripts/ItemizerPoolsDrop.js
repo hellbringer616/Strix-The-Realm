88f71cfd0047b46ab7ac883b4d958ace6e172fc4
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-20 19:51:50 -0500

    Update ItemizerPoolsDrop.js
    and it works

--- CustomNPCs Scripts/ItemizerPoolsDrop.js
+++ CustomNPCs Scripts/ItemizerPoolsDrop.js
@@ -2,8 +2,8 @@
 var itemService = Java.type("com.onaple.itemizer.utils.ItemService");
 //Anything within the brackets runs on the death of an NPC
 function dies(event) {
-//fetch the pool at each death
-var item = itemService.fetch(1)
+//retrieve an itemizer item
+var item = itemService.retrieve("1",1)//string of item ID, and quantity to receive
   // Instanciate the item at the location where the NPC dies
 itemService.instanciate(item,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ());
 }
