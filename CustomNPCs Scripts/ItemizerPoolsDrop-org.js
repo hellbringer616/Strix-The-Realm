d072932ab4afa3151dd6ff5852ea8c07820d2931
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-28 10:03:42 -0500

    item drop pools
    got item drop pools working; now i need to make the content.

--- CustomNPCs Scripts/ItemizerPoolsDrop-org.js
+++ CustomNPCs Scripts/ItemizerPoolsDrop-org.js
@@ -3,7 +3,7 @@ var itemService = Java.type("com.onaple.itemizer.utils.ItemService");
 //Anything within the brackets runs on the death of an NPC
 function died(event) {
 //retrieve an itemizer item
-var item = itemService.retrieve("1",1)//string of item ID, and quantity to receive
-  // Instanciate the item at the location where the NPC dies
+var item = itemService.fetch("1")//fetches an item from the itemizer item pool string of pool ID
+  //Instanciate the item at the location where the NPC dies
 itemService.instanciate(item,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ());
 }
