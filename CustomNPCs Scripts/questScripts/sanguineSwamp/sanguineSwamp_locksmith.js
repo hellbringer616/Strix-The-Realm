904f8940ff42065950efd853f12f108165dd409f
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-28 09:16:39 -0500

    Update sanguineSwamp_locksmith.js
    now using itemizer.js scripts

--- CustomNPCs Scripts/questScripts/sanguineSwamp/sanguineSwamp_locksmith.js
+++ CustomNPCs Scripts/questScripts/sanguineSwamp/sanguineSwamp_locksmith.js
@@ -1,15 +1,11 @@
-//Loads a seperate API, in this case Itemizers ItemService
-var itemService = Java.type("com.onaple.itemizer.utils.ItemService");
-
 function interact(event) {
-  var spongePlayer = event.player.getMCEntity(); //gets forge/sponge player entity
-  var playerName = event.player.getName(); //gets players name
-  var hasIronIngot = itemService.hasItem(spongePlayer,"22",1)//checks the players inventory for itemizer item id 22 with quantity of 1
+  var spongePlayer = _mc(event.player) //gets forge/sponge player entity
+  var hasIronIngot = Itemizer.hasItemizerItem(spongePlayer, "22", 1)//checks the players inventory for itemizer item id 22 with quantity of 1
 
       if(event.player.hasActiveQuest(17) && hasIronIngot){
         event.player.showDialog(49,"Josiah Cadwell"); //opens a dialog window for the player
-        itemservice.removeItem(spongePlayer,"22",1); //removes itemizer item with id 22 quantity 1 from players inventory
-        event.npc.executeCommand("retreive 9501 "+ playerName + ""); //gives an itemizer item
+        Itemizer.removeItemizerItem(spongePlayer,"22",1); //removes itemizer item with id 22 quantity 1 from players inventory
+        Itemizer.addItemizerItem(spongePlayer,"9501",1); //gives an itemizer item
         event.player.removeQuest(17); //removes a quest from the player
         event.player.finishQuest(17); //completes a quest
         event.player.startQuest(18); //starts a quest
