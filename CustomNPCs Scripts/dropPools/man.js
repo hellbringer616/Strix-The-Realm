f4cad7849935e1cc5234319443811eddab9a6373
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-28 19:14:47 -0500

    fixes
    cows don't drop money

--- CustomNPCs Scripts/dropPools/man.js
+++ CustomNPCs Scripts/dropPools/man.js
@@ -2,11 +2,13 @@
 var itemService = Java.type("com.onaple.itemizer.utils.ItemService");
 //Anything within the brackets runs on the death of an NPC
 function died(event) {
-var playerName = event.source.getName(); //gets the interacting players name
-var bones = itemService.retrieve("800",1)
-var pool = itemService.fetch("1")//fetches an item from the itemizer item pool string of pool ID
-  //Instanciate the item at the location where the NPC dies
-event.npc.executeCommand("adminpay " + playerName + " 3");
-itemService.instanciate(bones,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ()); //create dropped item at location
-itemService.instanciate(pool,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ()); //create dropped item at location
+    var playerName = event.source.getName(); //gets the interacting players name
+    var econLoot = 3
+    var bones = itemService.retrieve("800",1)
+    var pool = itemService.fetch("1")//fetches an item from the itemizer item pool string of pool ID
+        event.npc.executeCommand("adminpay " + playerName + " " + econLoot + "");
+
+      //Instanciate the item at the location where the NPC dies
+        itemService.instanciate(bones,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ()); //create dropped item at location
+        itemService.instanciate(pool,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ()); //create dropped item at location
 }
