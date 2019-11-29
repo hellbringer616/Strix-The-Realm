098c4195b07e5fe3478c2d72306c2630e90ef36b
hellbringer616 <m.johnsondelta@gmail.com>
2019-11-29 18:12:50 -0500

    Update bindings.js
    Maybe make a custom function?

--- config/nt-rpg/scripts/bindings.js
+++ config/nt-rpg/scripts/bindings.js
@@ -1,5 +1,5 @@
 /*
-With CNPC-NTRPG plugin CustomNPCs scripts have access to all NT-RPG scripts bindings. 
+With CNPC-NTRPG plugin CustomNPCs scripts have access to all NT-RPG scripts bindings.
 To give access to new variables or functions, use this
 
 var yourThing = {...};
@@ -15,3 +15,11 @@ Bindings.getScriptEngine().put("Sponge", Java.type("org.spongepowered.api.Sponge
 // nt-rpg things
 Bindings.getScriptEngine().put("RpgPlugin", Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin"));
 Bindings.getScriptEngine().put("GlobalScope", RpgPlugin.GlobalScope);
+
+//random number between numbers generator
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+Bindings.getScriptEngine().put("getRandomInt",getRandomInt)
