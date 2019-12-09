094d1dfed26cbbc4705f938f0c7005b588bb9b1b
hellbringer616 <m.johnsondelta@gmail.com>
2019-12-08 22:43:44 -0500

    Econ fixes
    Fixed up Sponge Economy Bridge

--- CustomNPCs Scripts/deathLoot.js
+++ CustomNPCs Scripts/deathLoot.js
@@ -3,10 +3,10 @@ var itemService = Java.type("com.onaple.itemizer.utils.ItemService");
 //Anything within the brackets runs on the death of an NPC
 function died(event) {
 //retrieve an itemizer item
-var killerName = event.source.getName();
 var item = itemService.retrieve("800",1)//string of item ID, and quantity to receive [Bones, quantity of 1]
-var econLoot = 3
-  // Instanciate the item at the location where the NPC dies
-  itemService.instanciate(item,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ());
-  event.npc.executeCommand("adminpay " + killerName + " " + econLoot); //play the player a value of Total Economey currency
+var uid = event.source.getUUID(); //get UUID of player that killed the NPC.
+var econLoot = 3 //must be an integer.
+
+  itemService.instanciate(item,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ()); // Instanciate the item at the location where the NPC dies
+  spongeEconomyDeposit(uid,econLoot) //Depoist a value into the players account through Sponge Economy Service; see bindings.js for details
 }
--- CustomNPCs Scripts/sponge-cpncBridge.js
+++
@@ -1,2 +0,0 @@
-var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin") //loads NTRPG Plugin
-var JavaUUID = Java.type('java.util.UUID') //Magic?
