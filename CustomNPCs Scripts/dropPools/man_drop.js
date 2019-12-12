2ce2da3aecae14fe2cb3e2b711a79a94e7d524b4
hellbringer616 <m.johnsondelta@gmail.com>
2019-12-12 17:05:03 -0500

    man drops
    drop tables for men

--- CustomNPCs Scripts/dropPools/man_drop.js
+++ CustomNPCs Scripts/dropPools/man_drop.js
@@ -5,6 +5,7 @@ function died(event) {
     var uid = event.source.getUUID(); //get UUID of player that killed the NPC.
     var rng = Math.floor((Math.random() * 100) + 1); //generates a random number between 1 and 100
     var bones = itemService.retrieve("800",1);
+    var pool0 = itemService.fetch("3");//fetches an item from the itemizer item pool string of pool ID
 
     ///*/// if rng greater than or equal to 30 do some math to get the amount of coins given to the player.
         if(rng >= 30){
@@ -16,4 +17,9 @@ function died(event) {
 
       //Instanciate the item at the location where the NPC dies
         itemService.instanciate(bones,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ()); //create dropped item at location
+
+      //checks if pool0 returns an item, if yes perform item drop.
+        if(pool0){
+          itemService.instanciate(pool0,event.npc.getWorld().getName(),event.npc.getBlockX(),event.npc.getBlockY(),event.npc.getBlockZ()); //create dropped item at location
+        }
 }
