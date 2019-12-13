6b153a5053b32ede7872dec40b5e384d7bc690ee
hellbringer616 <m.johnsondelta@gmail.com>
2019-12-13 17:30:38 -0500

    quest menu stuff
    make it load

--- config/placeholderapi/javascript/quests/questsanguineSwamp.js
+++ config/placeholderapi/javascript/quests/questsanguineSwamp.js
@@ -1,8 +1,8 @@
 var main = function (){
-var cnpcAPI = Java.type("noppes.npcs.api.NpcAPI").Instance()
-var cnpcPlayer = cnpcAPI.getIEntity(player);
-var out = "&cNot Started";
+  var cnpcAPI = Java.type("noppes.npcs.api.NpcAPI").Instance() //loads CustomNPC API
+  var cnpcPlayer = cnpcAPI.getIEntity(player); //gets Player entity from CustomNPC
 
+  var out = "&cNot Started";
   if(cnpcPlayer.hasActiveQuest(15) || cnpcPlayer.hasActiveQuest(16) || cnpcPlayer.hasActiveQuest(17) || cnpcPlayer.hasActiveQuest(18)){
     var out = "&eNot Completed";
   }
--- config/placeholderapi/javascript/quests/questthievescant.js
+++ config/placeholderapi/javascript/quests/questthievescant.js
@@ -1,9 +1,8 @@
 var main = function (){
-var cnpcAPI = Java.type("noppes.npcs.api.NpcAPI").Instance()
-var cnpcPlayer = cnpcAPI.getIEntity(player);
-  if(!cnpcPlayer.hasActiveQuest(13) && !cnpcPlayer.hasActiveQuest(14)){
-    var out = "&cNot Started";
-  }
+  var cnpcAPI = Java.type("noppes.npcs.api.NpcAPI").Instance() //loads CustomNPC API
+  var cnpcPlayer = cnpcAPI.getIEntity(player); //gets Player entity from CustomNPC
+
+  var out = "&cNot Started";
   if(cnpcPlayer.hasActiveQuest(13) || cnpcPlayer.hasActiveQuest(14)){
     var out = "&eNot Completed";
   }
