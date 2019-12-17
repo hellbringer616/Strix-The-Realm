05d96395e80567f9bdfa551c969c522675c0646a
hellbringer616 <m.johnsondelta@gmail.com>
2019-12-16 21:58:29 -0500

    menu rework
    reworked character menu scripts to reduce their number

--- config/placeholderapi/javascript/metallurgyxp.js
+++ config/placeholderapi/javascript/metallurgyxp.js
@@ -1,15 +1,16 @@
 //loads NTRPG Plugin
 var main = function() {
 var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
-  var className = "Weaponsmith" //must be a string
+  var className = "Metallurgy" //must be a string
   var uid = player.getUniqueId(); //Get Player UUID
   var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
   var classData = character.getClasses().get(className); //gets class data from character
   if(classData !== null) { //checks if classData returns null
     var levels = classData.getClassDefinition().getLevelProgression().getLevelMargins();
-    var expnextlevel = levels[classData.getLevel()]
+    var expnextlevel = Math.round(levels[classData.getLevel()])
+    var currentexp = Math.round(classData.getExperiencesFromLevel());
 
-	var out = Math.round(expnextlevel);
+	var out = "&6"+currentexp+" &5/ &6"+expnextlevel+""
 
 	return out;
   }
