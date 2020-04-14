9a7aa55708192cb974c5fe9653339eb51f08b592
hellbringer616 <m.johnsondelta@gmail.com>
2020-04-14 18:32:43 -0400

    Update bindings.js
    test

--- config/nt-rpg/scripts/bindings.js
+++ config/nt-rpg/scripts/bindings.js
@@ -11,8 +11,14 @@ Bindings.getScriptEngine().put("UUID", Java.type("java.util.UUID"));
 Bindings.getScriptEngine().put("BigDecimal", Java.type("java.math.BigDecimal"));
 
 // sponge things
-Bindings.getScriptEngine().put("Sponge", Java.type("org.spongepowered.api.Sponge"));
+var sponge = Java.type("org.spongepowered.api.Sponge");
+Bindings.getScriptEngine().put("Sponge", sponge);
+        sponge.getScheduler()
+                .createTaskBuilder()
+                .delay(3000, timeunits.MILLISECONDS)
+                ["execute(Runnable)"](function (t) {
 Bindings.getScriptEngine().put("EconomyService",Sponge.getServiceManager().provideUnchecked(Java.type("org.spongepowered.api.service.economy.EconomyService").class));
+        }).submit(RpgPlugin.GlobalScope.plugin);
 
 // nt-rpg things
 Bindings.getScriptEngine().put("RpgPlugin", Java.type("cz.neumimto.rpg.sponge.SpongeRpgPlugin"));
