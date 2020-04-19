463bdff8472395f6cca0f8fb1929a181b8eb976b
hellbringer616 <m.johnsondelta@gmail.com>
2020-04-19 12:18:19 -0400

    PAPI fixes
    fixes some scripts

--- config/placeholderapi/javascript/metallurgyxp.js
+++ config/placeholderapi/javascript/metallurgyxp.js
@@ -1,9 +1,10 @@
 //loads NTRPG Plugin
 var main = function() {
 var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.SpongeRpgPlugin");
+var RpgApi = Java.type("cz.neumimto.rpg.api.Rpg").get();
   var className = "Metallurgy" //must be a string
   var uid = player.getUniqueId(); //Get Player UUID
-  var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
+  var character = RpgApi.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
   var classData = character.getClasses().get(className); //gets class data from character
   if(classData !== null) { //checks if classData returns null
     var levels = classData.getClassDefinition().getLevelProgression().getLevelMargins();
