8f4b0b734efda0b8d11e4228c48cb9475b0579b6
hellbringer616 <m.johnsondelta@gmail.com>
2020-04-19 12:30:17 -0400

    PAPI script fixes
    its getCharacterService
    not characterService

--- config/placeholderapi/javascript/combatxp.js
+++ config/placeholderapi/javascript/combatxp.js
@@ -4,7 +4,7 @@ var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.SpongeRpgPlugin");
 var RpgApi = Java.type("cz.neumimto.rpg.api.Rpg").get();
   var className = "Combat" //must be a string
   var uid = player.getUniqueId(); //Get Player UUID
-  var character = RpgApi.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
+  var character = RpgApi.getCharacterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
   var classData = character.getClasses().get(className); //gets class data from character
   if(classData !== null) { //checks if classData returns null
     var levels = classData.getClassDefinition().getLevelProgression().getLevelMargins();
