786ff9db82c7a29eba0893f2afa7618c804abe8a
hellbringer616 <m.johnsondelta@gmail.com>
2020-06-03 14:06:55 -0400

    Update test_2.js
    Added notes to reference when writing documentation

--- config/nt-rpg/scripts/test_2.js
+++ config/nt-rpg/scripts/test_2.js
@@ -1,19 +1,19 @@
 registerSkillHandler('strix:alchemy',{
   onCast: function(character, context) {
-    var player = character.getPlayer();
-    var playerName = character.getPlayer().getName();
+    var player = character.getPlayer(); //gets the player entity
+    var playerName = character.getPlayer().getName(); //gets the name of the player
     //Config for items to craft with Itemizer
-    var item1 = context.getLevelNodeValue("comp1");
-    var item1qty = context.getLevelNodeValue("comp1qty");
-    var item2 = context.getLevelNodeValue("comp2");
-    var item2qty = context.getLevelNodeValue("comp2qty");
-    var item3 = context.getLevelNodeValue("comp3");
-    var item3qty = context.getLevelNodeValue("comp3qty");
-    var result = context.getLevelNodeValue("craftresult");
-    var resultqty = context.getLevelNodeValue("craftresultqty");
+    var item1 = context.getLevelNodeValue("comp1"); //Checks SkillSettings in the Skilltree for the node that matches the string
+    var item1qty = context.getLevelNodeValue("comp1qty"); //Checks SkillSettings in the Skilltree for the node that matches the string
+    var item2 = context.getLevelNodeValue("comp2"); //Checks SkillSettings in the Skilltree for the node that matches the string
+    var item2qty = context.getLevelNodeValue("comp2qty"); //Checks SkillSettings in the Skilltree for the node that matches the string
+    var item3 = context.getLevelNodeValue("comp3"); //Checks SkillSettings in the Skilltree for the node that matches the string
+    var item3qty = context.getLevelNodeValue("comp3qty"); //Checks SkillSettings in the Skilltree for the node that matches the string
+    var result = context.getLevelNodeValue("craftresult"); //Checks SkillSettings in the Skilltree for the node that matches the string
+    var resultqty = context.getLevelNodeValue("craftresultqty"); //Checks SkillSettings in the Skilltree for the node that matches the string
     //Class and XP config
     var ntclass = "Alchemy";
-    var xp = context.getLevelNodeValue("xpAmount");
+    var xp = context.getLevelNodeValue("xpAmount"); //Checks SkillSettings in the Skilltree for the node that matches the string
 
     //Assume that player inventory is empty
       if(Itemizer.hasItemizerItem(player, item1, item1qty) && Itemizer.hasItemizerItem(player, item2, item2qty) && Itemizer.hasItemizerItem(player, item3, item3qty)) {
